0b08a7d820514199d1f7a2322d83f036259208d9
kumar <santanupaikaray23@gmail.com>
2021-10-31 11:12:19 +0530

    add all files

--- apiWithOutUi.js
+++ apiWithOutUi.js
@@ -1,16 +1,14 @@
 const express = require ('express');
-// const app = express();
+const app = express();
 
 var router = express.Router();
-// const port = process.env.PORT || 9800;
-// const mongo = require ('mongodb')
-// const MongoClient = mongo.MongoClient;
+const port = process.env.PORT || 9800;
+const mongo = require ('mongodb')
+const MongoClient = mongo.MongoClient;
 const bodyParser = require ('body-parser')
-const Booked = require('./bookedSchema')
-
-// const cors = require ('cors');
-//  app.use(cors())
-// const mongourl ="mongodb+srv://naturewithcode:nature123@cluster0.yf62c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const cors = require ('cors');
+ app.use(cors())
+const mongourl ="mongodb+srv://naturewithcode:nature123@cluster0.yf62c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 let db;
 let col_name = "freelanceapi"
@@ -20,19 +18,19 @@ let col_name = "freelanceapi"
  let col_name4="freelanceapi4"
  let col_name5="freelanceapi5"
 //  let col_name6="freelanceapi6"
- let col_name6="Booked"
+ let col_name6="freelanceapi6"
 //  let col_name6="freelanceapi6"
 
-router.use(bodyParser.urlencoded({extended:true}))
-router.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
-router.get('/health',(req,res)=>{
+app.get('/health',(req,res)=>{
     res.status(200).send('Health Check')
 
 });
 
 //Read
-router.get('/service',(req,res)=>{
+app.get('/service',(req,res)=>{
     var query ={}
     if(req.query.service){
         query={service:req.query.service,isActive:true}
@@ -46,7 +44,7 @@ router.get('/service',(req,res)=>{
     })
 })
 //userDetails
-router.get('/Service/:id',(req,res)=>{
+app.get('/Service/:id',(req,res)=>{
     var id = mongo.ObjectId(req.params.id)
     db.collection(col_name).find({_id:id}).toArray((err,result)=>{
         if(err)throw err;
@@ -54,7 +52,7 @@ router.get('/Service/:id',(req,res)=>{
     })
 })
 //Insert
-router.post('/addService',(req,res)=>{
+app.post('/addService',(req,res)=>{
     console.log(req.body)
     db.collection(col_name).insert(req.body,(err,result)=>{
         if(err) throw err;
@@ -62,7 +60,7 @@ router.post('/addService',(req,res)=>{
     })
 })
 //update
-router.put('/updateService',(req,res)=>{
+app.put('/updateService',(req,res)=>{
     db.collection(col_name).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -79,7 +77,7 @@ router.put('/updateService',(req,res)=>{
     )
 })
 //delet
-router.delete('/deleteService',(req,res)=>{
+app.delete('/deleteService',(req,res)=>{
     db.collection(col_name)
     .remove({_id:mongo.ObjectId(req.body._id)},(err,result)=>{
         if(err) throw err;
@@ -87,7 +85,7 @@ router.delete('/deleteService',(req,res)=>{
     })
 })
 //softDelete(deactivate)
-router.put('/deactivateService',(req,res)=>{
+app.put('/deactivateService',(req,res)=>{
     db.collection(col_name).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -101,7 +99,7 @@ router.put('/deactivateService',(req,res)=>{
     )
 })
 //softDelete(activate)
-router.put('/activateService',(req,res)=>{
+app.put('/activateService',(req,res)=>{
     db.collection(col_name).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -117,7 +115,7 @@ router.put('/activateService',(req,res)=>{
 
 // services type
 // Read
-router.get('/types',(req,res)=>{
+app.get('/types',(req,res)=>{
     var query={}
     if(req.query.service){
         query={service:req.query.service,isActive:true}
@@ -134,7 +132,7 @@ router.get('/types',(req,res)=>{
 })
 
 //userDetails
-router.get('/types/:id',(req,res)=>{
+app.get('/types/:id',(req,res)=>{
     var id = mongo.ObjectId(req.params.id)
     db.collection(col_name2).find({_id:id}).toArray((err,result)=>{
         if(err) throw err;
@@ -143,7 +141,7 @@ router.get('/types/:id',(req,res)=>{
 
 })
 //Add
-router.post('/addType',(req,res)=>{
+app.post('/addType',(req,res)=>{
     console.log(req.body)
     db.collection(col_name2).insert(req.body,(err,result)=>{
         if(err) throw err;
@@ -151,7 +149,7 @@ router.post('/addType',(req,res)=>{
     })
 })
 //update
-router.put('/updateType',(req,res)=>{
+app.put('/updateType',(req,res)=>{
     db.collection(col_name2).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -175,7 +173,7 @@ router.put('/updateType',(req,res)=>{
 })
 
 //delet
-router.delete('/deleteType',(req,res)=>{
+app.delete('/deleteType',(req,res)=>{
     db.collection(col_name2)
     .remove({_id:mongo.ObjectId(req.body._id)},(err,result)=>{
       if(err)throw err;
@@ -183,7 +181,7 @@ router.delete('/deleteType',(req,res)=>{
     })
 })
 //softDelete(deactivate)
-router.put('/deactivateType',(req,res)=>{
+app.put('/deactivateType',(req,res)=>{
     db.collection(col_name2).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -200,7 +198,7 @@ router.put('/deactivateType',(req,res)=>{
     )
 })
 //activateDelete(activate)
-router.put('/activateType',(req,res)=>{
+app.put('/activateType',(req,res)=>{
     db.collection(col_name2).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -218,14 +216,14 @@ router.put('/activateType',(req,res)=>{
 })
 
 // Booking type
-router.get('/booking',(req,res)=>{
+app.get('/booking',(req,res)=>{
     db.collection(col_name3).find().toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
 })
 //Insert
-router.post('/addBooking',(req,res)=>{
+app.post('/addBooking',(req,res)=>{
     console.log(req.body)
     db.collection(col_name3).insert(req.body,(err,result)=>{
         if(err) throw err;
@@ -234,7 +232,7 @@ router.post('/addBooking',(req,res)=>{
 })
 
 // Read
-router.get('/servicelists',(req,res)=>{
+app.get('/servicelists',(req,res)=>{
     db.collection(col_name4).find().toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
@@ -243,7 +241,7 @@ router.get('/servicelists',(req,res)=>{
 
 
 //Add
-router.post('/addServicelist',(req,res)=>{
+app.post('/addServicelist',(req,res)=>{
     console.log(req.body)
     db.collection(col_name4).insert(req.body,(err,result)=>{
         if(err) throw err;
@@ -254,7 +252,7 @@ router.post('/addServicelist',(req,res)=>{
 
 
 //update
-router.put('/updateServicelist',(req,res)=>{
+app.put('/updateServicelist',(req,res)=>{
     db.collection(col_name4).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -275,7 +273,7 @@ router.put('/updateServicelist',(req,res)=>{
 })
 
 //delet
-router.delete('/deleteServicelist',(req,res)=>{
+app.delete('/deleteServicelist',(req,res)=>{
     db.collection(col_name4)
     .remove({_id:mongo.ObjectId(req.body._id)},(err,result)=>{
       if(err)throw err;
@@ -285,7 +283,7 @@ router.delete('/deleteServicelist',(req,res)=>{
 
 
 // Read
-router.get('/servicedetails',(req,res)=>{
+app.get('/servicedetails',(req,res)=>{
     var query={}
      query={isActive:true}
         db.collection(col_name5).find(query).toArray((err,result)=>{
@@ -295,7 +293,7 @@ router.get('/servicedetails',(req,res)=>{
 
 })
 //Read
-router.get('/servicedetail/:service',(req,res)=>{
+app.get('/servicedetail/:service',(req,res)=>{
     
     var service =(req.params.service)
     db.collection(col_name5).find({service:service}).toArray((err,result)=>{
@@ -306,7 +304,7 @@ router.get('/servicedetail/:service',(req,res)=>{
 })
 
 //Read
-router.get('/servicedetails/:id',(req,res)=>{
+app.get('/servicedetails/:id',(req,res)=>{
     var id = (req.params.id)
     db.collection(col_name5).find({_id:id}).toArray((err,result)=>{
         if(err) throw err;
@@ -315,7 +313,7 @@ router.get('/servicedetails/:id',(req,res)=>{
 })
 
 //Add
-router.post('/addServicedetail',(req,res)=>{
+app.post('/addServicedetail',(req,res)=>{
     console.log(req.body)
     db.collection(col_name5).insert(req.body,(err,result)=>{
         if(err) throw err;
@@ -323,7 +321,7 @@ router.post('/addServicedetail',(req,res)=>{
     })
 })
 //update
-router.put('/updateServicedetail',(req,res)=>{
+app.put('/updateServicedetail',(req,res)=>{
     db.collection(col_name5).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -356,7 +354,7 @@ router.put('/updateServicedetail',(req,res)=>{
     )
 })
 //delete
-router.delete('/deleteServicedetail',(req,res)=>{
+app.delete('/deleteServicedetail',(req,res)=>{
     db.collection(col_name5).remove({_id:mongo.ObjectId(req.body._id)},(err,result)=>{
         if(err) throw err;
         res.send('Data Deleted')
@@ -364,7 +362,7 @@ router.delete('/deleteServicedetail',(req,res)=>{
 })
 
     
-router.put('/deactivateServicedetail',(req,res)=>{
+app.put('/deactivateServicedetail',(req,res)=>{
     db.collection(col_name5).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -378,7 +376,7 @@ router.put('/deactivateServicedetail',(req,res)=>{
     )
 })
 //softDelete(activate)
-router.put('/activateServicedetail',(req,res)=>{
+app.put('/activateServicedetail',(req,res)=>{
     db.collection(col_name5).updateOne(
         {_id:mongo.ObjectId(req.body._id)},
         {
@@ -392,7 +390,7 @@ router.put('/activateServicedetail',(req,res)=>{
     )
 })
 //Read
-router.get('/placeBooking',(req,res)=>{
+app.get('/placeBooking',(req,res)=>{
 db.collection(col_name6).find().toArray((err,result)=>{
     if(err) throw err;
     res.send(result)
@@ -400,7 +398,7 @@ db.collection(col_name6).find().toArray((err,result)=>{
 
 })
 //Insert
-router.post('/addPlaceBooking',(req,res)=>{
+app.post('/addPlaceBooking',(req,res)=>{
     console.log(req.body)
     db.collection(col_name6).insert(req.body,(err,result) => {
         if(err) throw err;
@@ -409,14 +407,13 @@ router.post('/addPlaceBooking',(req,res)=>{
 })
 
 
-// app.use("/",router)
+app.use("/",router)
 
-// //Db Connection
-// MongoClient.connect(mongourl,(err,client)=>{
-//     if(err) console.log('Error while connecting');
-//     db=client.db('octobernode');
-//     app.listen(port,(err)=>{
-//         console.log(`Server is running on port ${port}`)
-//     })
-// })
-module.exports = router;
+//Db Connection
+MongoClient.connect(mongourl,(err,client)=>{
+    if(err) console.log('Error while connecting');
+    db=client.db('octobernode');
+    app.listen(port,(err)=>{
+        console.log(`Server is running on port ${port}`)
+    })
+})
--- db.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require ('mongoose');
-mongoose.connect('mongodb+srv://naturewithcode:nature123@cluster0.yf62c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
