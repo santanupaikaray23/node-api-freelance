0486e3838eb9452ca440fd159627a4afb84d8500
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-05-20 11:54:50 +0530

    add all changes

--- apiWithOutUi.js
+++ apiWithOutUi.js
@@ -27,20 +27,44 @@ app.use(bodyParser.json())
 app.get('/health',(req,res)=>{
     res.status(200).send('Health Check')
 })
-//Read
 app.get('/contact',(req,res)=>{
     db.collection(col_name9).find().toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
 })
-
-//Insert
+    
+// Insert
 app.post('/addcontacts',(req,res)=>{
     console.log(req.body)
     db.collection(col_name9).insert(req.body,(err,result)=>{
-        if(err) throw err;
-        res.send('Data Added')
+        if(err){
+            console.log(err);
+            return res.status(500).send('Error adding data');
+        }
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user: 'santanupaikaray1996@gmail.com',
+                pass: 'aghr bhwz lxii uvzo'
+            }
+        })
+        var mailOptions = {
+            from: 'santanupaikaray1996@gmail.com',
+            to: req.body.email,
+            subject: 'Thank you for Contacting Santanu Paikaray',
+            text: 'Hi, \n \n I got your Message. \n \n I will get back you after some time. \n \n \n Regards,\n Santanu Paikaray \n Phone no. 8917310896',
+
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            return res.status(500).send('Error sending email');
+        }else{
+            console.log("email sent" + info.response);
+            return res.send('Data Added and Email Sent')
+        }
+        })
     })
 })
 
