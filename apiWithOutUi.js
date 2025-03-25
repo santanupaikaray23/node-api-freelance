c23f92937380169f4aecd29b8a21471c32ece989
santanupaikaray23 <santanupaikaray1996@gmail.com>
2025-03-25 10:38:20 +0530

    add all files

--- apiWithOutUi.js
+++ apiWithOutUi.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 9600;
+const port = process.env.PORT || 9601;
 const mongo = require('mongodb');
 // const MongoClient = mongo.MongoClient;
 const MongoClient = require('mongodb').MongoClient;
@@ -11,16 +11,9 @@ const cors = require('cors')
 app.use(cors())
 const mongourl = "mongodb+srv://portfolio:portfolio1996@cluster0.yf62c.mongodb.net/?retryWrites=true&w=majority";
 let db;
-// let col_name1 = "col1"
-// let col_name2 = "col2"
-// let col_name3 = "col3"
-// let col_name4 = "col4"
-// let col_name5 = "col5"
-// let col_name6 = "col6"
-// let col_name7 = "col7"
-let col_name8 = "col8"
-let col_name9 = "col9"
-let col_name10 = "col10"
+let col_name3 = "col3"
+
+
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
@@ -29,359 +22,46 @@ app.get('/health',(req,res)=>{
     res.status(200).send('Health Check')
 })
 
-
-
-//Read
-// app.get('/contacts',(req,res)=>{
-//     db.collection(col_name3).find().toArray((err,result)=>{
-//         if(err) throw err;
-//         res.send(result)
-//     })
-// })
-
-// // Insert
-// app.post('/addcontacts',(req,res)=>{
-//     console.log(req.body)
-//     db.collection(col_name3).insert(req.body,(err,result)=>{
-//         if(err){
-//             console.log(err);
-//             return res.status(500).send('Error adding data');
-//         }
-//         var transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth:{
-//                 user: 'nwithcode@gmail.com',
-//                 pass: 'sdfg ddfw tuui ofsq'
-//             }
-//         })
-//         var mailOptions = {
-//             from: 'nwithcode@gmail.com',
-//             to: req.body.email,
-//             subject: 'ଆମକୁ Contact କରିଥିବାରୁ ଆପଣଙ୍କୁ ବହୁତ ଧନ୍ୟବାଦ !!',
-//             text: 'Hi, \n \n ଆମେ ଆପଣଙ୍କ Message ପାଇଲୁ । \n \n  ଆମେ ଆପଣଙ୍କୁ ବହୁ ଶୀଘ୍ର Contract କରିବୁ । \n \n \n Regards,\n Nature With Code Team \n Phone no. 9668348106',
-            
-//         };
-//         transporter.sendMail(mailOptions, function(error, info){
-//         if(error){
-//             console.log(error);
-//             return res.status(500).send('Error sending email');
-//         }else{
-//             console.log("email sent" + info.response);
-//             return res.send('Data Added and Email Sent')
-//         }
-//         })
-//     })
-// })
-
-//Read
-// app.get('/users',(req,res)=>{
-//     db.collection(col_name5).find().toArray((err,result)=>{
-//         if(err) throw err;
-//         res.send(result)
-//     })
-// })
-
-// // Insert
-// app.post('/addUsers',(req,res)=>{
-//     console.log(req.body)
-//     db.collection(col_name5).insert(req.body,(err,result)=>{
-//         if(err){
-//             console.log(err);
-//             return res.status(500).send('Error adding data');
-//         }
-//         var transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth:{
-//                 user: 'nwithcode@gmail.com',
-//                 pass: 'sdfg ddfw tuui ofsq'
-//             }
-//         })
-//         var mailOptions = {
-//             from: 'nwithcode@gmail.com',
-//             to: req.body.email,
-//             subject: 'ଆପଣଙ୍କ Enquiry ପାଇଁ ବହୁତ ଧନ୍ୟବାଦ !!',
-//             text: 'Hi, \n \n ଆମେ ଆପଣଙ୍କ Enquiry Request ପାଇଲୁ । \n \n ଆମେ ଆପଣଙ୍କୁ ବହୁ ଶୀଘ୍ର Contract କରିବୁ । \n \n \n Regards,\n Nature With Code Team \n Phone no. 9668348106',
-
-
-
-
-//         };
-//         transporter.sendMail(mailOptions, function(error, info){
-//         if(error){
-//             console.log(error);
-//             return res.status(500).send('Error sending email');
-//         }else{
-//             console.log("email sent" + info.response);
-//             return res.send('Data Added and Email Sent')
-//         }
-//         })
-//     })
-// })
-
-//Read
-// app.get('/services',(req,res)=>{
-//     db.collection(col_name1).find().toArray((err,result)=>{
-//         if(err) throw err;
-//         res.send(result)
-//     })
-// })
+app.get('/contacts',(req,res)=>{
+    db.collection(col_name3).find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
 
 // Insert
-// app.post('/addservices',(req,res)=>{
-//     console.log(req.body)
-//     db.collection(col_name1).insert(req.body,(err,result)=>{
-//         if(err) throw err;
-//         res.send('Data Added')
-//     })
-// })
-
-//  app.get('/servicesdetails',(req,res) => {
-//      var query = {}
-//     query= {isActivate:true}
-//      db.collection(col_name4).find().toArray((err,result)=>{
-//         if(err) throw err;
-//          res.send(result)
-//      })
-//  })
-
-//  app.get('/servicesdetails',(req,res) => {
-//     var query = {}
-//   if(req.query.name){
-//    query={name:req.query.name,isActive:true}
-//   }else{
-//    query = {isActive:true}
-//   }
-//   db.collection(col_name4).find(query).toArray((err,result)=>{
-//    if(err) throw err;
-//    res.send(result)
-
-//   })
-// })
-
-// //serviceDetails
-// app.get('/servicesdetail/:servicetypeId', (req, res) => {
-//     // Convert personId from string to number as it's stored as a number in your data
-//     const servicetypeId = parseInt(req.params.servicetypeId);
-
-//     db.collection(col_name4).find({"servicetype._id": servicetypeId}).toArray((err, result) => {
-//         if (err) {
-//             res.status(500).send('Error fetching data');
-//             return;
-//         }
-//         res.send(result);
-//     });
-// });
-
-
-
-// //Insert
-// app.post('/addservicesdetail',(req,res)=>{
-//     console.log(req.body)
-//     db.collection(col_name4).insert(req.body,(err,result)=>{
-//         if(err) throw err;
-//         res.send('Data Added')
-//     })
-// })
-
-
-
-
-// // //Read
-
-// // app.get('/separatetype',(req,res)=>{
-// //      db.collection(col_name3).find().toArray((err,result)=>{
-// //          if(err) throw err;
-// //          res.send(result)
-// //      })
-// //  })
-
-// //  //Insert
-// //  app.post('/addseparatetype',(req,res)=>{
-// //      console.log(req.body)
-// //      db.collection(col_name3).insert(req.body,(err,result)=>{
-// //          if(err) throw err;
-// //          res.send('Data Added')
-// //      })
-// //  })
-
-// //  app.get('/check/:separatetypeId', (req, res) => {
-// //     // Convert personId from string to number as it's stored as a number in your data
-// //     const separatetypeId = parseInt(req.params.separatetypeId);
-
-// //     db.collection(col_name3).find({"separatetype._id": separatetypeId}).toArray((err, result) => {
-// //         if (err) {
-// //             res.status(500).send('Error fetching data');
-// //             return;
-// //         }
-// //         res.send(result);
-// //     });
-// // });
-
-// // //update
-// app.put('/updateservices',(req,res)=>{
-//     db.collection(col_name1).updateOne(
-//         {_id:mongo.ObjectID(req.body._id)},
-//         {
-//             $set:{
-           
-//     name: req.body.name,
-//     email: req.body.email,
-//     formData: req.body.formData,
-//     formData: req.body.forceUpdatePwd
-//             }
-//         },(err,result)=>{
-//             if(err) throw err;
-//             res.send('Data Updated')
-//         }
-//     )
-// })
-
-// //delete
-// app.delete('/deleteservices',(req,res)=>{
-//     db.collection(col_name1).remove({_id:mongo.ObjectID(req.body._id)},(err,result)=>{
-//         if(err) throw err;
-//         res.send('Data Deleted')
-
-//     })
-
-// })
-// //softDelete(deactivate)
-// app.put('/deactivateservices',(req,res)=>{
-//     db.collection(col_name1).updateOne(
-//         {_id:mongo.ObjectID(req.body._id)},
-//         {
-//             $set:{
-//     isActivate: false
-//             }
-//         },(err,result)=>{
-//             if(err) throw err;
-//             res.send('service deactivated')
-//         }
-//     )
-// })
-// //activateDelete(activate)
-// app.put('/deactivateservices',(req,res)=>{
-//     db.collection(col_name1).updateOne(
-//         {_id:mongo.ObjectID(req.body._id)},
-//         {
-//             $set:{
-//     isActivate: true
-//             }
-//         },(err,result)=>{
-//             if(err) throw err;
-//             res.send('service activated')
-//         }
-//     )
-// })
-
-// app.get('/booking',(req,res)=>{
-//     db.collection(col_name2).find().toArray((err,result)=>{
-//         if(err) throw err;
-//         res.send(result)
-//     })
-// })
-
-// //Insert
-// app.post('/addbooking',(req,res)=>{
-//     console.log(req.body)
-//     db.collection(col_name2).insert(req.body,(err,result)=>{
-//         if(err){
-//             console.log(err);
-//             return res.status(500).send('Error adding data');
-//         }
-//         var transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth:{
-//                 user: 'nwithcode@gmail.com',
-//                 pass: 'sdfg ddfw tuui ofsq'
-//             }
-//         })
-//         var mailOptions = {
-//             from: 'nwithcode@gmail.com',
-//             to: req.body.email,
-//             subject: 'ଆପଣ Successfully ରେଜିଷ୍ଟର ହୋଇଛନ୍ତି !!',
-//             text: 'Hi, \n \n ଆମ ସର୍ଭିସ ବାଛିଥିବାରୁ ଆପଣଙ୍କୁ ବହୁତ ଧନ୍ୟବାଦ । \n \n ଆପଣ Successfully ରେଜିଷ୍ଟର ହୋଇଛନ୍ତି । \n \n ଆମେ ଆପଣଙ୍କୁ ବହୁ ଶୀଘ୍ର Contract କରିବୁ । \n \n \n Regards,\n Nature With Code Team \n Phone no. 9668348106',
-
-
-
-
-//         };
-//         transporter.sendMail(mailOptions, function(error, info){
-//         if(error){
-//             console.log(error);
-//             return res.status(500).send('Error sending email');
-//         }else{
-//             console.log("email sent" + info.response);
-//             return res.send('Data Added and Email Sent')
-//         }
-//         })
-//     })
-// })
-
-// app.get('/payment-details',(req,res)=>{
-//         db.collection(col_name7).find().toArray((err,result)=>{
-//             if(err) throw err;
-//             res.send(result)
-//         })
-//     })
-    
-//      app.post('/api/payment-details',(req,res)=>{
-//         console.log(req.body)
-//         db.collection(col_name7).insert(req.body,(err,result)=>{
-//             if(err) throw err;
-//             res.send('Data Added')
-//         })
-//     })
-
-app.get('/tablesdata',(req,res)=>{
-    db.collection(col_name8).find().toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-//Insert
-app.post('/addtablesdata',(req,res)=>{
+app.post('/addcontacts',(req,res)=>{
     console.log(req.body)
-    db.collection(col_name8).insert(req.body,(err,result)=>{
-        if(err) throw err;
-        res.send('Data Added')
+    db.collection(col_name3).insert(req.body,(err,result)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).send('Error adding data');
+        }
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user: 'nwithcode@gmail.com',
+                pass: 'sdfg ddfw tuui ofsq'
+            }
+        })
+        var mailOptions = {
+            from: 'nwithcode@gmail.com',
+            to: req.body.email,
+            subject: 'ମତେ Contact କରିଥିବାରୁ ଆପଣଙ୍କୁ ବହୁତ ଧନ୍ୟବାଦ !!',
+            text: 'Hi There, \n \n ମୁଁ ଆପଣଙ୍କ Message ପାଇଲି । \n \n ମୁଁ ଆପଣଙ୍କୁ ବହୁ ଶୀଘ୍ର Contract କରିବି । \n \n \n Regards,\n ସାନ୍ତନୁ କୁମାର ପାଇକରାୟ \n Phone no. 8917310896',
+            
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            return res.status(500).send('Error sending email');
+        }else{
+            console.log("email sent" + info.response);
+            return res.send('Data Added and Email Sent')
+        }
+        })
     })
 })
-
-app.get('/forms',(req,res)=>{
-    db.collection(col_name9).find().toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-//Insert
-app.post('/addforms', async (req, res) => {
-    console.log(req.body)
-    db.collection(col_name9).insert(req.body,(err,result)=>{
-        // if(err) throw err;
-        res.status(200).json({ success: true, message: 'Data Added Successfully' });
-    })
-   
-  });
-
-app.get('/state',(req,res)=>{
-    db.collection(col_name10).find().toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-app.post('/addstate', async (req, res) => {
-    console.log(req.body)
-    db.collection(col_name10).insert(req.body,(err,result)=>{
-        // if(err) throw err;
-        res.status(200).json({ success: true, message: 'Data Added Successfully' });
-    })
-   
-  });
-
 
 
 
